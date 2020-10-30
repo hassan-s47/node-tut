4441ade336a3ed5d0e6de678a261185c3069f770
hassan-s47 <hhsheikh77@gmail.com>
2020-10-30 12:09:25 +0500

    middleware

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
 const express = require('express');
+const morgan = require('morgan');
 
-//express app
+// express app
 const app = express();
 
-//register ejs
-app.set('view engine','ejs');
-
-//running the app
+// listen for requests
 app.listen(3000);
 
-//handling get requests
+// register view engine
+app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
+// middleware & static files
+app.use(express.static('public'));
 
-  blogs = [
-    {title:"ABC",snippet:"qwoieufsdmfkls ijrweoi wejksdfl wefwef"},
-    {title:"SDJE",snippet:"qwoieufsdmfkls ijrweoi wejksdfl wefwef"},
-    {title:"ASKD",snippet:"qwoieufsdmfkls ijrweoi wejksdfl wefwef"}
-  ];
-  res.render('index',blogs); 
-
+app.use((req, res, next) => {
+  console.log('new request made:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
 });
 
+app.use((req, res, next) => {
+  console.log('in the next middleware');
+  next();
+});
 
-app.get('/about',(req,res)=>{
+app.use(morgan('dev'));
 
-    res.render('about',{name:'HS'}); 
-  
-  });
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
-  app.get('/blogs/create',(req,res)=>{
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
 
-    res.render('create'); 
-    
-  });
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
 
-//handling 404s
-app.use( (req,res) => {
-    res.status(404).render('404');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
